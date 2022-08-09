bf4e990c8d33a630d1bfaee2de544175f435c398
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-08-10 02:55:16 +0700

    first project success to run

--- 1-FirstAppiumProject/src/test/java/appiumtests/CalculatorTest.java
+++ 1-FirstAppiumProject/src/test/java/appiumtests/CalculatorTest.java
@@ -28,15 +28,23 @@ public class CalculatorTest {
 	public static void openCalculator() {
 		try {
 			DesiredCapabilities cap = new DesiredCapabilities();
-
+			
+			//AOSP on IA
 			// device data
-			cap.setCapability("deviceName", "ASUS_Z01QD");
-			cap.setCapability("udid", "127.0.0.1:21503");
+			cap.setCapability("deviceName", "AOSP on IA Emulator");
+			cap.setCapability("udid", "emulator-5554");
 			cap.setCapability("platformName", "Android");
-			cap.setCapability("platformVersion", "7.1.2");
-
+			cap.setCapability("platformVersion", "9");
+			
+/*
+ * Name: Calculator
+Package: com.android.calculator2
+Signature: f3:8a:b5:f3:f8:93:8c:5c:8c:84:79:4e:64:3d:e8:93:fd:c4:1b:18
+Version name: 9
+ Version Code: 28
+ */
 			// App data
-			cap.setCapability("appPackage", "com.google.android.calculator");
+			cap.setCapability("appPackage", "com.android.calculator2");
 			cap.setCapability("appActivity", "com.android.calculator2.Calculator");
 
 			URL url = new URL("http://127.0.0.1:4723/wd/hub");
