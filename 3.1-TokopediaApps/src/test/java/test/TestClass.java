87d4dceb3222481a6ef9bfdb826c3c5c6ace9371
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-09-01 21:00:19 +0700

    new test project

---
+++ 3.1-TokopediaApps/src/test/java/test/TestClass.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriverException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+
+public class TestClass extends BaseClass {
+
+	@Test(priority=1)
+	public void onBoardingPage() {
+		// Mobile Element
+		MobileElement GabungSekarangBtn = driver.findElement(By.id("com.tokopedia.tkpd.df_base:id/buttonGlobalDynamicOnbaording"));
+		MobileElement LewatiSekarangBtn = driver.findElement(By.id("com.tokopedia.tkpd.df_base:id/skipDynamicOnbaording"));
+		MobileElement SelanjutnyaBtn = driver.findElement(By.id("com.tokopedia.tkpd.df_base:id/nextDynamicOnbaording"));
+		
+		//Assert
+		assertTrue(GabungSekarangBtn.isDisplayed());
+		assertTrue(LewatiSekarangBtn.isDisplayed());
+		assertTrue(SelanjutnyaBtn.isDisplayed());
+
+	}
+	
+	@Test(priority=2)
+	public void homePage() throws Exception {
+		// Mobile Element
+		Thread.sleep(1000);
+		MobileElement LewatiBtn = driver.findElement(By.id("com.tokopedia.tkpd.df_base:id/skipDynamicOnbaording"));
+		LewatiBtn.click();
+		
+		MobileElement HomeBtn = driver.findElement(By.id("com.tokopedia.tkpd:id/menu_home"));
+		//Assert
+		assertTrue(HomeBtn.isDisplayed());
+
+	}
+	
+
+}
